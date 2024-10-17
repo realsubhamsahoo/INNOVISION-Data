8889582edc61e0e4fc89f3c928ee1d7aa232b208
Subham Sahoo <realsubhamsahoo@gmail.com>
2024-10-17 20:21:41 +0530

    Inno Data

---
+++ EventsPage/FlagshipEvents/Data.js
@@ -0,0 +1,132 @@
+export const FE_previewItems = [
+    {
+      id: 1,
+      ImageURL: 'https://picsum.photos/500',
+      PreviewDescription:
+        'Get ready for Death Race, a two-day robotics competition where participants drive their robots through a track filled with obstacles and challenges. The fastest robot to navigate the course and score the most points wins. Brace yourself for an intense and thrilling showdown!',
+    },
+    {
+      id: 2,
+      ImageURL: 'https://picsum.photos/600',
+      PreviewDescription:
+        'Aeroprix, the flagship aeronautic event of Innovision 2024, brings together the sharpest minds in RC aviation. Prepare to be amazed as teams from across the country showcase their engineering prowess, flying state-of-the-art RC aircrafts in a high-stakes competition that tests creativity and precision.',
+    },
+    {
+      id: 3,
+      ImageURL: 'https://picsum.photos/400',
+      PreviewDescription:
+        'Unleash your moves in The Hustlemania 3.0.This thrilling freestyle dance event features Solo and Duo categories, each with Prelims and Final rounds where participants bring their best in just 60 seconds. The intensity peaks in the final round with a head-to-head face-off on random music tracks.',
+    },
+    {
+      id: 4,
+      ImageURL: 'https://picsum.photos/600',
+      PreviewDescription:
+        'Embark on an immersive journey with Game Showcase, a virtual reality game and campus tour designed to transport you into a stunning digital landscape where exploration meets adventure. Explore the enormous campus of NIT Rourkela virtually through Oculus Virtual Reality headsets and experience your surroundings like never before.',
+    },
+    {
+      id: 5,
+      ImageURL: 'https://picsum.photos/500',
+      PreviewDescription:
+        'Embark on a thrilling adventure through a maze built inside a tent! Along the way, solve questions at checkpoints to progress. Your goal? Find and conquer as many checkpoints as possible before time runs out. The participants with the most checkpoints and correct answers will claim victory. Test your problem-solving, strategy, and navigation in this exciting race against time!',
+    },
+    {
+      id: 6,
+      ImageURL: 'https://picsum.photos/600',
+      PreviewDescription:
+        'The Ultimate Treasure Hunt is a thrill ing adventure where logic, strategy, and math skills are your keys to success. Teams will embark on a campus-wide hunt, so ving puzzles and cracking codes that lead to hidden treasures. Each step tests problem-solving, creativity, and teamwork. The clues will challenge your mathematical limits while keeping the excitement high.',
+    },
+    {
+      id: 7,
+      ImageURL: 'https://picsum.photos/400',
+      PreviewDescription:
+        'The Algorithm and Programming Society of NITR is back with its flagship event of INNOVISION, Meteor Madness, an interact ive game involving a logrithmic problems and brainstorming quizzes that will enthral the participants in an adrenaline-filled atmosphere, testing their cognitive as well as coding skills. Presenting a cosmos of challenging problems at different levels, th is competition will be both exciting and unforgettable. Dont miss out!',
+    },
+    {
+      id: 8,
+      ImageURL: 'https://picsum.photos/500',
+      PreviewDescription:
+        'Join Stellar Night, a stargazing event by Astro NITR. Observe celestial objects like the Moon, Mars, Saturn, Jupiter, and its moons through powerful telescopes. Learn about astronomy and have a fun night under the stars!',
+    },
+  ];
+  
+  export const FE_descriptionItems = [
+    {
+      id: 1,
+      Heading: 'DEATHRACE',
+      Description:
+        ' "In the race of innovation, speed is not just an advantage"; it’s the only option. Get ready for Death Race, a two-day robotics competition where participants drive their robots through a track filled with obstacles and challenges. The fastest robot to navigate the course and score the most points wins. Brace yourself for an intense and thrilling showdown!',
+      ImageURL: 'https://picsum.photos/800',
+      Location: 'SAC (In front of SAC stairs)',
+        Date: 'Day 2, Day 3',
+        Prizes: '₹40000',
+    },
+    {
+      id: 2,
+      Heading: 'AEROPRIX',
+      Description:
+        '"When passion meets the skies, innovation soars!" Aeroprix, the flagship aeronautic event of Innovision 2024, brings together the sharpest minds in RC aviation. Prepare to be amazed as teams from across the country showcase their engineering prowess, flying state-of-the-art RC aircrafts in a high-stakes competition that tests creativity and precision.',
+      ImageURL: 'https://picsum.photos/900',
+      Location: 'STS Ground',
+        Date: 'Day 2',
+        Prizes: '₹15000',
+    },
+    {
+      id: 3,
+      Heading: 'HUSTLEMANIA 3.O',
+      Description:
+        ' “There are shortcuts to happiness, and dancing is one of them.” Unleash your moves in The Hustlemania 3.0.This thrilling freestyle dance event features Solo and Duo categories, each with Prelims and Final rounds where participants bring their best in just 60 seconds. The intensity peaks in the final round with a head-to-head face-off on random music tracks. Whether you\'re dancing alone or with a partner, step up and show off your freestyle magic—because there’s no greater feeling than dancing to the rhythm and letting the world disappear!',
+      Location: '',
+        Date: '',
+        Prizes: '₹10000',
+    },
+    {
+      id: 4,
+      Heading: 'Campus Survival: The VR Expedition',
+      Description:
+        'Step into the shoes of the last remaining heroes in Campus Survival: The VR Expedition. The world is on the brink of collapse, and the only hope lies within the NIT Rourkela campus. Your mission: retrieve the antidote and save humanity. With danger lurking around every corner, it\'s up to you and your teammate to navigate the campus, outsmart threats, and complete the mission. This is more than just a game-it\'s a pulse-pounding adventure where every second counts. Are you ready to be the last survivors?',
+      ImageURL: 'https://picsum.photos/600',
+      Location: '',
+        Date: '',
+        Prizes: '₹6000',
+    },
+    {
+      id: 5,
+      Heading: 'MAZEHUNT',
+      Description:
+        'Step into a thrilling adventure as you navigate through a maze constructed within a tent. Along the way, participants will encounter checkpoints where they must solve questions to continue their journey. The objective? Find and conquer as many checkpoints as possible before time runs out! The participants who discover the most checkpoints and answer the questions correctly will emerge victorious. Get ready to test your problem-solving skills, strategy, and sense of direction in this exciting race against time!',
+      ImageURL: 'https://picsum.photos/800',
+      Location: 'LA-1 Cycle Parking',
+        Date: '',
+        Prizes: '₹8000',
+    },
+    {
+      id: 6,
+      Heading: 'OMEGA',
+      Description:
+        'Get ready for Death Race, a two-day robotics competition where participants drive their robots through a track filled with obstacles and challenges. The fastest robot to navigate the course and score the most points wins. Brace yourself for an intense and thrilling showdown!',
+      ImageURL: 'https://picsum.photos/900',
+      Location: 'LA Lawns, LA 004, LA 009, Academic Area',
+        Date: 'Day 2',
+        Prizes: '₹8000',
+    },
+    {
+      id: 7,
+      Heading: 'METEOR MADNESS',
+      Description:
+        'The Algorithm and Programming Society of NITR is back with its flagship event of INNOVISION, Meteor Madness, an interactive game involving algorithmic problems and brainstorming quizzes that will enthral the participants in an adrenaline-filled atmosphere, testing their cognitive as well as coding skills. Presenting a cosmos of challenging problems at different levels, this competition will be both exciting and unforgettable. Don\'t miss out!',
+      ImageURL: 'https://picsum.photos/800',
+      Location: '',
+        Date: '',
+        Prizes: '₹8000',
+    },
+    {
+      id: 8,
+      Heading: 'STELLAR NIGHT',
+      Description:
+        'Get ready to embark on a magical journey through the cosmos with Stellar Night, brought to you by Astro NITR! As one of the signature events of Innovision, Stellar Night offers an unforgettable celestial experience, where the wonders of the universe come to life. With high-powered telescopes provided by the club—two CPC automated and one manual—you’ll have the rare opportunity to observe stunning sights like the Moon, Mars, Saturn, Jupiter, and its four Galilean moons. Whether you\'re an astronomy enthusiast or just curious about the night sky, Stellar Night promises an awe-inspiring blend of stargazing, science, and entertainment. Join us for a night under the stars, and let the universe amaze you!',
+      ImageURL: 'https://picsum.photos/600',
+      Location: 'LA-1',
+        Date: '',
+        Prizes: '',
+    },
+  ];
