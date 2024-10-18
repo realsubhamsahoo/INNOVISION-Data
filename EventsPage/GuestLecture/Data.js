ab2270f4dd50b4208d9c7a2e4f631ab50f68aa1a
Subham Sahoo <realsubhamsahoo@gmail.com>
2024-10-18 07:30:37 +0530

    Guest Lectures Add

--- EventsPage/GuestLecture/Data.js
+++ EventsPage/GuestLecture/Data.js
@@ -3,63 +3,92 @@ export const GL_previewItems = [
       id: 1,
       ImageURL: 'https://picsum.photos/500',
       PreviewDescription:
-        'IRIDESCENCE is the flagship event hosted by DESIGN TAB, the official design club of NIT Rourkela, as part of INNOVISION. This exhibition is a vibrant celebration of creativity...',
+        '"Late-night study sessions may blur, but the impact of Physics Wallah is unforgettable." E-Cell proudly presents Alakh Pandey, who began as a YouTuber and transformed into the founder of Physics Wallah, one of India’s most beloved educational platforms...',
     },
     {
       id: 2,
       ImageURL: 'https://picsum.photos/600',
       PreviewDescription:
-        'We invite you to RADIANCE 6.0, an elaborate art and craft exhibition organised and hosted by Akriti, the official art and craft club of NITR. Every year...',
+        'The Food Tech Society is organizing a 1.5-hour Guest Lecture, aimed at providing valuable insights into the latest advancements and trends in the food technology industry...',
     },
     {
       id: 3,
       ImageURL: 'https://picsum.photos/400',
       PreviewDescription:
-        'Get ready for some serious innovation at Little Science Yard (LSY), brought to you by AASRA! Students across Rourkela will gather to show off their coolest science projects...',
+        'The Astro Guest Lecture/Workshop on Astrophotography is an engaging event led by a renowned astrophysicist or celebrated astrophotographer. The session explores advanced techniques...',
     },
     {
       id: 4,
       ImageURL: 'https://picsum.photos/600',
       PreviewDescription:
-        'SRIJAN 7.0 is a main attraction of INNOVISION, organized by the Students of Planning and Architecture, featuring a diverse range of interactive art installations, drawings, photographs, paintings, optical illusions...',
+        'Join us as an industry expert delves into the fascinating world of NLP, the technology behind AI\'s ability to understand, interpret, and generate human language. From chatbots and virtual assistants to machine translation...',
     },
+    {
+        id: 5,
+        ImageURL: 'https://picsum.photos/600',
+        PreviewDescription:
+          'Get ready for some serious innovation at Little Science Yard (LSY), brought to you by AASRA! Students across Rourkela will gather to show off their coolest science projects, packed with creativity and brainpower...',
+      },
+      {
+        id: 6,
+        ImageURL: 'https://picsum.photos/600',
+        PreviewDescription:
+          'Join us for an inspiring and insightful session as we host a guest lecture by a prominent personality in the Tech Domain.Attendees will gain valuable perspectives on the evolution of tech...',
+      },
   ];
   
   export const GL_descriptionItems = [
     {
       id: 1,
-      Heading: 'IRIDESCENSE',
+      Heading: 'Guest Lecture - ALAKH PANDEY',
       Description:
-        'IRIDESCENCE is the flagship event hosted by DESIGN TAB, the official design club of NIT Rourkela, as part of INNOVISION. This exhibition is a vibrant celebration of creativity, showcasing an impressive array of artworks, including graphic illustrations, 3D models, typographic arts, crafts, fine arts, and more—all crafted by our talented club members. This year, IRIDESCENCE promises to be more dynamic, interactive, and visually captivating than ever, ensuring a memorable experience. Join us for this euphoric journey through the world of art!',
+        '"Late-night study sessions may blur, but the impact of Physics Wallah is unforgettable." E-Cell proudly presents Alakh Pandey, who began as a YouTuber and transformed into the founder of Physics Wallah, one of India’s most beloved educational platforms. His journey from teaching online to creating a revolutionary ed-tech platform is a story of determination and innovation. Be ready to draw inspiration from his incredible rise.',
       ImageURL: 'https://picsum.photos/800',
-      Location: '',
-      Date: ''
+      Location: 'BBA',
+      Date: 'DAY 2'
     },
     {
       id: 2,
-      Heading: 'RADIANCE 6.0',
+      Heading: 'Guest Lecture - FOOD TECH SOCIETY',
       Description:
-        'We invite you to RADIANCE 6.0, an elaborate art and craft exhibition organised and hosted by Akriti, the official art and craft club of NITR. Every year, Radiance serves as a platform for artists to express their artistic visions and experiment with new techniques, bringing together a vibrant array of artworks of different art forms, including paintings, sculptures, handmade crafts and more. Join us for this inspiring experience and immerse yourself In the world of art!',
+        'The Food Tech Society is organizing a 1.5-hour Guest Lecture, aimed at providing valuable insights into the latest advancements and trends in the food technology industry. This lecture offers a unique opportunity to learn from an expert in the field, fostering knowledge and sparking interest in innovative food tech solutions.',
       ImageURL: 'https://picsum.photos/900',
       Location: '',
       Date: ''
     },
     {
       id: 3,
-      Heading: 'LITTLE SCIENCE YARD',
+      Heading: 'Guest Lecture - ASTRONITR',
       Description:
-        'Get ready for some serious innovation at Little Science Yard (LSY), brought to you by AASRA! Students across Rourkela will gather to show off their coolest science projects, packed with creativity and brainpower. With a panel of judges on hand, only the most mind-blowing ideas will make it to the top and score those winning spots. Think you\'ve got what it takes to impress?',
+        'The Astro Guest Lecture/Workshop on Astrophotography is an engaging event led by a renowned astrophysicist or celebrated astrophotographer. The session explores advanced techniques for capturing the night sky, covering the use of telescopes, cameras, and image processing software. Participants, from beginners to experts, gain practical insights to enhance their astrophotography skills. A Q&A session allows direct interaction with the guest speaker, making this workshop a unique opportunity to learn from a leading figure and capture the beauty of the cosmos.',
       ImageURL: 'https://picsum.photos/800',
-      Location: 'Chemical Dept Parking',
+      Location: '',
       Date: ''
     },
     {
       id: 4,
-      Heading: 'SRIJAN 7.0',
+      Heading: 'Guest Lecture - NLP Proficiency ML4E',
       Description:
-        'SRIJAN 7.0 is a main attraction of INNOVISION, organized by the Students of Planning and Architecture, featuring a diverse range of interactive art installations, drawings, photographs, paintings, optical illusions and much more. These exhibits highlight architectural creativity, exploring various styles, forms, and techniques and delving into the connections and harmony between different architectural expressions and environments. Join us for SRIJAN 7.0 and experience euphroria!',
+        'Join us as an industry expert delves into the fascinating world of NLP, the technology behind AI\'s ability to understand, interpret, and generate human language. From chatbots and virtual assistants to machine translation and sentiment analysis, discover how NLP is revolutionizing industries and shaping the future of communication, this session is a must-attend for anyone eager to explore the cutting-edge of artificial intelligence.',
       ImageURL: 'https://picsum.photos/600',
-      Location: 'RM HALL',
+      Location: 'PPA',
       Date: ''
     },
+    {
+        id: 5,
+        Heading: 'Guest Lecture - ANALYTICS & CONSULTING',
+        Description:
+          'Get ready for some serious innovation at Little Science Yard (LSY), brought to you by AASRA! Students across Rourkela will gather to show off their coolest science projects, packed with creativity and brainpower. With a panel of judges on hand, only the most mind-blowing ideas will make it to the top and score those winning spots. Think you\'ve got what it takes to impress?',
+        ImageURL: 'https://picsum.photos/800',
+        Location: '',
+        Date: ''
+      },
+      {
+        id: 6,
+        Heading: 'Guest Lecture - APS',
+        Description: 'Join us for an inspiring and insightful session as we host a guest lecture by a prominent personality in the Tech Domain.Attendees will gain valuable perspectives on the evolution of tech and the skills required to thrive in this fast-changing landscape of technology and innovation.',
+        ImageURL: 'https://picsum.photos/800',
+        Location: 'BBA',
+        Date: 'DAY 3'
+      },
   ];
