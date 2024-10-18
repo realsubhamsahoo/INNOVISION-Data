032ae1e376f57097773e0e4152c7917463398be0
Subham Sahoo <realsubhamsahoo@gmail.com>
2024-10-18 17:19:12 +0530

    DTS Shows Coming Soon Update

--- EventsPage/DTS_Shows/Data.js
+++ EventsPage/DTS_Shows/Data.js
@@ -1,65 +1,65 @@
 export const DTS_previewItems = [
     {
       id: 1,
-      ImageURL: 'https://picsum.photos/500',
+      ImageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw_HeSzHfBorKS4muw4IIeVvvRgnhyO8Gn8w&s',
       PreviewDescription:
-        'IRIDESCENCE is the flagship event hosted by DESIGN TAB, the official design club of NIT Rourkela, as part of INNOVISION. This exhibition is a vibrant celebration of creativity...',
+        'Coming Soon...',
     },
     {
       id: 2,
-      ImageURL: 'https://picsum.photos/600',
+      ImageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw_HeSzHfBorKS4muw4IIeVvvRgnhyO8Gn8w&s',
       PreviewDescription:
-        'We invite you to RADIANCE 6.0, an elaborate art and craft exhibition organised and hosted by Akriti, the official art and craft club of NITR. Every year...',
+        'Coming Soon...',
     },
     {
       id: 3,
-      ImageURL: 'https://picsum.photos/400',
+      ImageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw_HeSzHfBorKS4muw4IIeVvvRgnhyO8Gn8w&s',
       PreviewDescription:
-        'Get ready for some serious innovation at Little Science Yard (LSY), brought to you by AASRA! Students across Rourkela will gather to show off their coolest science projects...',
+        'Coming Soon...',
     },
     {
       id: 4,
-      ImageURL: 'https://picsum.photos/600',
+      ImageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw_HeSzHfBorKS4muw4IIeVvvRgnhyO8Gn8w&s',
       PreviewDescription:
-        'SRIJAN 7.0 is a main attraction of INNOVISION, organized by the Students of Planning and Architecture, featuring a diverse range of interactive art installations, drawings, photographs, paintings, optical illusions...',
+        'Coming Soon...',
     },
   ];
   
   export const DTS_descriptionItems = [
     {
       id: 1,
-      Heading: 'IRIDESCENSE',
+      Heading: 'Coming Soon...',
       Description:
-        'IRIDESCENCE is the flagship event hosted by DESIGN TAB, the official design club of NIT Rourkela, as part of INNOVISION. This exhibition is a vibrant celebration of creativity, showcasing an impressive array of artworks, including graphic illustrations, 3D models, typographic arts, crafts, fine arts, and more—all crafted by our talented club members. This year, IRIDESCENCE promises to be more dynamic, interactive, and visually captivating than ever, ensuring a memorable experience. Join us for this euphoric journey through the world of art!',
-      ImageURL: 'https://picsum.photos/800',
+        'Coming Soon...',
+      ImageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw_HeSzHfBorKS4muw4IIeVvvRgnhyO8Gn8w&s',
       Location: '',
       Date: ''
     },
     {
       id: 2,
-      Heading: 'RADIANCE 6.0',
+      Heading: 'Coming Soon...',
       Description:
-        'We invite you to RADIANCE 6.0, an elaborate art and craft exhibition organised and hosted by Akriti, the official art and craft club of NITR. Every year, Radiance serves as a platform for artists to express their artistic visions and experiment with new techniques, bringing together a vibrant array of artworks of different art forms, including paintings, sculptures, handmade crafts and more. Join us for this inspiring experience and immerse yourself In the world of art!',
-      ImageURL: 'https://picsum.photos/900',
+        'Coming Soon...',
+      ImageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw_HeSzHfBorKS4muw4IIeVvvRgnhyO8Gn8w&s',
       Location: '',
       Date: ''
     },
     {
       id: 3,
-      Heading: 'LITTLE SCIENCE YARD',
+      Heading: 'Coming Soon...',
       Description:
-        'Get ready for some serious innovation at Little Science Yard (LSY), brought to you by AASRA! Students across Rourkela will gather to show off their coolest science projects, packed with creativity and brainpower. With a panel of judges on hand, only the most mind-blowing ideas will make it to the top and score those winning spots. Think you\'ve got what it takes to impress?',
-      ImageURL: 'https://picsum.photos/800',
-      Location: 'Chemical Dept Parking',
+        'Coming Soon...',
+      ImageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw_HeSzHfBorKS4muw4IIeVvvRgnhyO8Gn8w&s',
+      Location: '',
       Date: ''
     },
     {
       id: 4,
-      Heading: 'SRIJAN 7.0',
+      Heading: 'Coming Soon...',
       Description:
-        'SRIJAN 7.0 is a main attraction of INNOVISION, organized by the Students of Planning and Architecture, featuring a diverse range of interactive art installations, drawings, photographs, paintings, optical illusions and much more. These exhibits highlight architectural creativity, exploring various styles, forms, and techniques and delving into the connections and harmony between different architectural expressions and environments. Join us for SRIJAN 7.0 and experience euphroria!',
-      ImageURL: 'https://picsum.photos/600',
-      Location: 'RM HALL',
+        'Coming Soon...',
+      ImageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw_HeSzHfBorKS4muw4IIeVvvRgnhyO8Gn8w&s',
+      Location: '',
       Date: ''
     },
   ];
