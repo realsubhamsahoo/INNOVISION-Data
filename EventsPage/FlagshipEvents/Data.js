ee4f18b54d1b88623fdd36a51d0530fa2344c7d5
Subham Sahoo <realsubhamsahoo@gmail.com>
2024-10-19 18:58:40 +0530

    TBA add

--- EventsPage/FlagshipEvents/Data.js
+++ EventsPage/FlagshipEvents/Data.js
@@ -58,7 +58,7 @@ export const FE_previewItems = [
       ImageURL: 'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729179008/inno-2024/Event%20Posters/Cyborg/DeathRace/ljimi3k8z9jb1ee75jgr.jpg',
       Location: 'SAC (In front of SAC stairs)',
         Date: 'Day 2, Day 3',
-        Time: '',
+        Time: 'TBA',
         Prizes: '₹40000',
     },
     {
@@ -69,7 +69,7 @@ export const FE_previewItems = [
       ImageURL: 'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729179013/inno-2024/Event%20Posters/UDAAN/v4yhrdzyfrok0vnweijv.png',
       Location: 'STS Ground',
         Date: 'Day 2',
-        Time: '',
+        Time: 'TBA',
         Prizes: '₹15000',
     },
     {
@@ -78,9 +78,9 @@ export const FE_previewItems = [
       Description:
         'Step into the shoes of the last remaining heroes in Campus Survival: The VR Expedition. The world is on the brink of collapse, and the only hope lies within the NIT Rourkela campus. Your mission: retrieve the antidote and save humanity. With danger lurking around every corner, it\'s up to you and your teammate to navigate the campus, outsmart threats, and complete the mission. This is more than just a game-it\'s a pulse-pounding adventure where every second counts. Are you ready to be the last survivors?',
       ImageURL: 'https://socialcry.com/wp-content/uploads/2024/10/dummy-image-lorem-ipsum.png600', // GTA To be updated
-      Location: '',
-        Date: '',
-        Time: '',
+      Location: 'TBA',
+        Date: 'TBA',
+        Time: 'TBA',
         Prizes: '₹6000',
     },
     {
@@ -90,8 +90,8 @@ export const FE_previewItems = [
         'Step into a thrilling adventure as you navigate through a maze constructed within a tent. Along the way, participants will encounter checkpoints where they must solve questions to continue their journey. The objective? Find and conquer as many checkpoints as possible before time runs out! The participants who discover the most checkpoints and answer the questions correctly will emerge victorious. Get ready to test your problem-solving skills, strategy, and sense of direction in this exciting race against time!',
       ImageURL: 'https://socialcry.com/wp-content/uploads/2024/10/dummy-image-lorem-ipsum.png', // GENESYS To be updated
       Location: 'LA-1 Cycle Parking',
-        Date: '',
-        Time: '',
+        Date: 'TBA',
+        Time: 'TBA',
         Prizes: '₹8000',
     },
     {
@@ -102,7 +102,7 @@ export const FE_previewItems = [
       ImageURL: 'https://socialcry.com/wp-content/uploads/2024/10/dummy-image-lorem-ipsum.png', // AXIOM To be updated
       Location: 'LA Lawns, LA 004, LA 009, Academic Area',
         Date: 'Day 2',
-        Time: '',
+        Time: 'TBA',
         Prizes: '₹8000',
     },
     {
@@ -111,9 +111,9 @@ export const FE_previewItems = [
       Description:
         'The Algorithm and Programming Society of NITR is back with its flagship event of INNOVISION, Meteor Madness, an interactive game involving algorithmic problems and brainstorming quizzes that will enthral the participants in an adrenaline-filled atmosphere, testing their cognitive as well as coding skills. Presenting a cosmos of challenging problems at different levels, this competition will be both exciting and unforgettable. Don\'t miss out!',
       ImageURL: 'https://socialcry.com/wp-content/uploads/2024/10/dummy-image-lorem-ipsum.png', // APS To be updated
-      Location: '',
-        Date: '',
-        Time: '',
+      Location: 'TBA',
+        Date: 'TBA',
+        Time: 'TBA',
         Prizes: '₹8000',
     },
     {
@@ -122,9 +122,9 @@ export const FE_previewItems = [
       Description:
         'Get ready to embark on a magical journey through the cosmos with Stellar Night, brought to you by Astro NITR! As one of the signature events of Innovision, Stellar Night offers an unforgettable celestial experience, where the wonders of the universe come to life. With high-powered telescopes provided by the club—two CPC automated and one manual—you’ll have the rare opportunity to observe stunning sights like the Moon, Mars, Saturn, Jupiter, and its four Galilean moons. Whether you\'re an astronomy enthusiast or just curious about the night sky, Stellar Night promises an awe-inspiring blend of stargazing, science, and entertainment. Join us for a night under the stars, and let the universe amaze you!',
       ImageURL: 'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729179001/inno-2024/Event%20Posters/Astro_NITR/clqps60ndso26fmw101a.png',
-      Location: '',
-        Date: '',
-        Time: '',
+      Location: 'TBA',
+        Date: 'TBA',
+        Time: '10 PM - 2 AM',
         Prizes: '',
     },
     {
@@ -134,8 +134,8 @@ export const FE_previewItems = [
           '"Chemistry has energy and it\'s meant to be felt." In this thrilling event, teams of up to 5 students are challenged to design and construct a miniature car powered solely by chemical reactions. From the moment it starts to its final stop, every aspect of the car\'s motion must be driven by chemical processes, with no electricity or manual intervention. This event challenges you to merge chemistry with engineering and problem-solving skills. Come experience the thrill of innovation as your creation races into action. ',
         ImageURL: 'https://socialcry.com/wp-content/uploads/2024/10/dummy-image-lorem-ipsum.png', // TBA
         Location: 'LA-Room for Day 01(for workshop) , Chemical Eng. Dept New Seminar Hall for Day 03: Chem-E-Car Test Run',
-          Date: 'Day 01(workshop)(02:00 PM - 05:00 PM), Day 03(Car Testing) (11:00 AM - 01:00 PM)',
-          Time: '',
-        Prizes: '',
+          Date: 'Day 01 (Workshop), Day 03 (Car Testing)',
+          Time: 'DAY 1: 2 PM - 5 PM, DAY 3: 11 AM - 1 PM',
+          Prizes: '',
       },
-  ];
+  ];c
