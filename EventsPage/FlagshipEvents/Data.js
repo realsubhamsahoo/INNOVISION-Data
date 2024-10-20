e6ede8d4314a6f633c453469ad8390ef304b2966
Subham Sahoo <131604843+realsubhamsahoo@users.noreply.github.com>
2024-10-20 13:59:28 +0530

    Flagship event Rulebook update

--- EventsPage/FlagshipEvents/Data.js
+++ EventsPage/FlagshipEvents/Data.js
@@ -4,49 +4,57 @@ export const FE_previewItems = [
       ImageURL: 'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729179008/inno-2024/Event%20Posters/Cyborg/DeathRace/ljimi3k8z9jb1ee75jgr.jpg',
       PreviewDescription:
         'Get ready for Death Race, a two-day robotics competition where participants drive their robots through a track filled with obstacles and challenges. The fastest robot to navigate the course and score the most points wins. Brace yourself for an intense and thrilling showdown!',
+        Rulebook: 'https://drive.google.com/open?id=12RIvja14ve3uInA9wwwztDACrvi3X_Ci'
     },
     {
       id: 2,
       ImageURL: 'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729179013/inno-2024/Event%20Posters/UDAAN/v4yhrdzyfrok0vnweijv.png',
       PreviewDescription:
         'Aeroprix, the flagship aeronautic event of Innovision 2024, brings together the sharpest minds in RC aviation. Prepare to be amazed as teams from across the country showcase their engineering prowess, flying state-of-the-art RC aircrafts in a high-stakes competition that tests creativity and precision.',
+        Rulebook: 'https://drive.google.com/file/d/1MLd5jP5KfvUjQsQkiAmo6EPU0KfQ7vF8/view?usp=drive_link'
     },
     {
       id: 3,
       ImageURL: 'https://socialcry.com/wp-content/uploads/2024/10/dummy-image-lorem-ipsum.png', // GTA (To be updated)
       PreviewDescription:
         'Embark on an immersive journey with Game Showcase, a virtual reality game and campus tour designed to transport you into a stunning digital landscape where exploration meets adventure. Explore the enormous campus of NIT Rourkela virtually through Oculus Virtual Reality headsets and experience your surroundings like never before.',
+      Rulebook: 'https://drive.google.com/open?id=1Z9zLd0CdDhMqT12tnjsPbe2ZRxI0qUva'
     },
     {
       id: 4,
       ImageURL: 'https://socialcry.com/wp-content/uploads/2024/10/dummy-image-lorem-ipsum.png', // GENESYS To be updated
       PreviewDescription:
         'Embark on a thrilling adventure through a maze built inside a tent! Along the way, solve questions at checkpoints to progress. Your goal? Find and conquer as many checkpoints as possible before time runs out. The participants with the most checkpoints and correct answers will claim victory. Test your problem-solving, strategy, and navigation in this exciting race against time!',
+      Rulebook: 'https://drive.google.com/open?id=1Kxt1qVCS6xK2qcX_63SQmRj-XQJkMy7g'
     },
     {
       id: 5,
       ImageURL: 'https://socialcry.com/wp-content/uploads/2024/10/dummy-image-lorem-ipsum.png', // AXIOM To be updated
       PreviewDescription:
         'The Ultimate Treasure Hunt is a thrill ing adventure where logic, strategy, and math skills are your keys to success. Teams will embark on a campus-wide hunt, so ving puzzles and cracking codes that lead to hidden treasures. Each step tests problem-solving, creativity, and teamwork. The clues will challenge your mathematical limits while keeping the excitement high.',
+      Rulebook: 'https://drive.google.com/open?id=1jy98okGN6yeKQJAuJlXvS59Qa1TpYWrh'
     },
     {
       id: 6,
       ImageURL: 'https://socialcry.com/wp-content/uploads/2024/10/dummy-image-lorem-ipsum.png', // APS To be updated
       PreviewDescription:
         'The Algorithm and Programming Society of NITR is back with its flagship event of INNOVISION, Meteor Madness, an interact ive game involving a logrithmic problems and brainstorming quizzes that will enthral the participants in an adrenaline-filled atmosphere, testing their cognitive as well as coding skills. Presenting a cosmos of challenging problems at different levels, th is competition will be both exciting and unforgettable. Dont miss out!',
+      Rulebook: 'https://drive.google.com/open?id=1RZcGSiwNF-0BOzjU67XngYXsNIRjVBp3'
     },
     {
       id: 7,
       ImageURL: 'https://res.cloudinary.com/dwfon2ikz/image/upload/v1729179001/inno-2024/Event%20Posters/Astro_NITR/clqps60ndso26fmw101a.png',
       PreviewDescription:
         'Join Stellar Night, a stargazing event by Astro NITR. Observe celestial objects like the Moon, Mars, Saturn, Jupiter, and its moons through powerful telescopes. Learn about astronomy and have a fun night under the stars!',
+      Rulebook: ''
     },
     {
         id: 8,
         ImageURL: 'https://socialcry.com/wp-content/uploads/2024/10/dummy-image-lorem-ipsum.png', //TBA
         PreviewDescription:
           '"Chemistry has energy and it\'s meant to be felt." In this thrilling event, teams of up to 5 students are challenged to design and construct a miniature car powered solely by chemical reactions.',
-      },
+        Rulebook: ''
+    },
   ];
   
   export const FE_descriptionItems = [
