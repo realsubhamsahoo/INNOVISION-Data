a92671fd3c2c4a45e2d51d10c2af1985f79b0238
dasbidyendu <143386290+dasbidyendu@users.noreply.github.com>
2024-10-20 12:14:45 +0530

    Update Data.js

--- EventsPage/Exhibition/Data.js
+++ EventsPage/Exhibition/Data.js
@@ -1,7 +1,7 @@
 export const Exhibition_previewItems = [
     {
       id: 1,
-      ImageURL: 'https://socialcry.com/wp-content/uploads/2024/10/dummy-image-lorem-ipsum.png',
+      ImageURL: 'https://res.cloudinary.com/dsse5djcy/image/upload/f_auto,q_auto/v1/INNO/zr00klxr3kup5pnapemg',
       PreviewDescription:
         'IRIDESCENCE is the flagship event hosted by DESIGN TAB, the official design club of NIT Rourkela, as part of INNOVISION. This exhibition is a vibrant celebration of creativity...',
     },
@@ -13,13 +13,13 @@ export const Exhibition_previewItems = [
     },
     {
       id: 3,
-      ImageURL: 'https://socialcry.com/wp-content/uploads/2024/10/dummy-image-lorem-ipsum.png',
+      ImageURL: 'https://res.cloudinary.com/dsse5djcy/image/upload/f_auto,q_auto/v1/INNO/ytzstb65zif8x2jnj8z4',
       PreviewDescription:
         'Get ready for some serious innovation at Little Science Yard (LSY), brought to you by AASRA! Students across Rourkela will gather to show off their coolest science projects...',
     },
     {
       id: 4,
-      ImageURL: 'https://socialcry.com/wp-content/uploads/2024/10/dummy-image-lorem-ipsum.png',
+      ImageURL: 'https://res.cloudinary.com/dsse5djcy/image/upload/f_auto,q_auto/v1/INNO/uqjjhsq1bl3nk7y2cbki',
       PreviewDescription:
         'SRIJAN 7.0 is a main attraction of INNOVISION, organized by the Students of Planning and Architecture, featuring a diverse range of interactive art installations, drawings, photographs, paintings, optical illusions...',
     },
@@ -31,7 +31,7 @@ export const Exhibition_previewItems = [
       Heading: 'IRIDESCENSE',
       Description:
         'IRIDESCENCE is the flagship event hosted by DESIGN TAB, the official design club of NIT Rourkela, as part of INNOVISION. This exhibition is a vibrant celebration of creativity, showcasing an impressive array of artworks, including graphic illustrations, 3D models, typographic arts, crafts, fine arts, and more—all crafted by our talented club members. This year, IRIDESCENCE promises to be more dynamic, interactive, and visually captivating than ever, ensuring a memorable experience. Join us for this euphoric journey through the world of art!',
-      ImageURL: 'https://socialcry.com/wp-content/uploads/2024/10/dummy-image-lorem-ipsum.png',
+      ImageURL: 'https://res.cloudinary.com/dsse5djcy/image/upload/f_auto,q_auto/v1/INNO/zr00klxr3kup5pnapemg',
       Location: 'TBA',
       Date: 'TBA',
       Time: 'TBA'
@@ -51,7 +51,7 @@ export const Exhibition_previewItems = [
       Heading: 'LITTLE SCIENCE YARD',
       Description:
         'Get ready for some serious innovation at Little Science Yard (LSY), brought to you by AASRA! Students across Rourkela will gather to show off their coolest science projects, packed with creativity and brainpower. With a panel of judges on hand, only the most mind-blowing ideas will make it to the top and score those winning spots. Think you\'ve got what it takes to impress?',
-      ImageURL: 'https://socialcry.com/wp-content/uploads/2024/10/dummy-image-lorem-ipsum.png',
+      ImageURL: 'https://res.cloudinary.com/dsse5djcy/image/upload/f_auto,q_auto/v1/INNO/ytzstb65zif8x2jnj8z4',
       Location: 'Chemical Dept Parking',
       Date: 'TBA',
       Time: 'TBA'
@@ -61,7 +61,7 @@ export const Exhibition_previewItems = [
       Heading: 'SRIJAN 7.0',
       Description:
         'SRIJAN 7.0 is a main attraction of INNOVISION, organized by the Students of Planning and Architecture, featuring a diverse range of interactive art installations, drawings, photographs, paintings, optical illusions and much more. These exhibits highlight architectural creativity, exploring various styles, forms, and techniques and delving into the connections and harmony between different architectural expressions and environments. Join us for SRIJAN 7.0 and experience euphroria!',
-      ImageURL: 'https://socialcry.com/wp-content/uploads/2024/10/dummy-image-lorem-ipsum.png',
+      ImageURL: 'https://res.cloudinary.com/dsse5djcy/image/upload/f_auto,q_auto/v1/INNO/uqjjhsq1bl3nk7y2cbki',
       Location: 'RM HALL',
       Date: 'TBA',
       Time: 'TBA'
